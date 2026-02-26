f8d5497689984ebc9f0b3f9642818e8532163fbd
Josh <josh@ghettoretro.com>
2026-02-26 07:53:30 -0500

    vite install

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,3 @@
-/**
- * @PATH [src/main.jsx]
- * @REV [20260225-0150]
- * @MODULE [OS]
- * @STATUS [DEV]
- * @FILETYPE [HOK]
- * @DESC ["Main application entry point for the Audio Asset Manager UI"]
- * @COMPLIANCE [None]
- * -------------------------------------
- * @TODO_START
- * @TODO_END
- * =====================================*/
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
